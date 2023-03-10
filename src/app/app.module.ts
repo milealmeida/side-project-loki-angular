5444c99b46b5b68f3c5215a05fef8483ac027fab
Milena Almeida <milenaalmeidadev@hotmail.com>
2023-03-10 19:42:49 -0400

    feat: create layout mobile and desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { InfoComponent } from './content/info/info.component';
+import { ButtonComponent } from './content/info/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    InfoComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
